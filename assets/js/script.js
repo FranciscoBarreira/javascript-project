03ab0ef5abd1b749093cd7c80702700f2694a950
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-01-25 18:06:32 +0000

    add js functions and start array of questions

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const randomQuestions, currentIndex 
+
+
+function startQuiz() {
+nextQuestion() 
+currentIndex = 0
+randomQuestions = questions.sort (() => Math.random() -0.5)
+}
+
+function nextQuestion() {
+
+}
+
+const questions = [
+    {
+        question: "In which movie series does Michael Mayer terrorize the town of?",
+        answers: [
+           { option: "Friday the 13th", correct: false},
+           { option: "Halloween", correct: true},
+           { option: "Saw", correct: false},
+           { option: "Texas Chainsaw Massacre", correct: false},
+        ]
+    }
+]
