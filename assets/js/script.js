607a19b927a6b40a061cd66302c3b9a38dc39d72
FranciscoBarreira <Francisco.barreira1992@gmail.com>
2022-01-27 15:32:23 +0000

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,19 @@ const questions = [
            { option: "Friday the 13th", correct: false},
            { option: "Halloween", correct: true},
            { option: "Saw", correct: false},
-           { option: "Texas Chainsaw Massacre", correct: false},
+           { option: "Texas Chainsaw Massacre", correct: false}
         ]
-    }
+    },
+
+    {
+        question: "In which movie series does Michael Mayer terrorize the town of?",
+        answers: [
+           { option: "Friday the 13th", correct: false},
+           { option: "Halloween", correct: true},
+           { option: "Saw", correct: false},
+           { option: "Texas Chainsaw Massacre", correct: false}
+        ]
+    },
+
+
 ]
